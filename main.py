6d0657ae5e0074f43314baac6f7e837b8ee25905
starscode61 <164739585+starscode61@users.noreply.github.com>
2025-12-15 15:02:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import json
+import tkinter as tk
+from tkinter import PhotoImage
+
+with open("eleves.json", "r", encoding="utf-8") as f:
+    base = json.load(f)
+
+carte_prof = None
+eleves = {}
+
+for identifiant, nom in base.items():
+    if nom == "Professeur":
+        carte_prof = identifiant
+    else:
+        eleves[identifiant] = nom
+
+presents = []
+
+
+def verifier(event):
+    id = entree.get()
+    entree.delete(0, tk.END)
+    entree.focus_set()
+
+    if id not in base:
+        resultat.config(text="Identifiant inconnu")
+        return
+
+    if id == carte_prof:
+        absents = []
+        for nom in eleves.values():
+            if nom not in presents:
+                absents.append(nom)
+
+        if len(absents) > 0:
+            resultat.config(text="Les élèves absents sont :\n" + "\n".join(absents))
+        else:
+            resultat.config(text="Tous les élèves sont présents")
+        return
+
+    nom = base[id]
+
+    if nom not in presents:
+        presents.append(nom)
+
+    resultat.config(text=f"{nom} est présent.")
+
+
+root = tk.Tk()
+root.geometry("500x300")
+root.title("Projet lecteur de cartes")
+logo = PhotoImage(file="logo.png")
+root.iconphoto(True, logo)
+
+label_info = tk.Label(root, text="Veuillez scanner votre carte", font=("Marianne", 16))
+label_info.pack(pady=10)
+
+entree = tk.Entry(root, font=("Marianne", 14))
+entree.pack(pady=10)
+entree.focus_set()
+
+entree.bind("<Return>", verifier)
+
+resultat = tk.Label(root, text="", font=("Marianne", 14))
+resultat.pack(pady=20)
+
+root.mainloop()
